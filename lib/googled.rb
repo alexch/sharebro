fbd4d50213b2bc6c5e0d7928fa87c8d77ff381e9
Alex Chaffee <alex@stinky.com>
2011-11-08 20:31:00 -0800

    friends list now has lots of feed links

--- lib/googled.rb
+++ lib/googled.rb
@@ -19,13 +19,38 @@ class Googled < Widget
         comment: "basic information like your user id and join date"
 
       span.saved "Saved!"
+      
+      widget Bro, :user_info => @user_info
+      
       pre @user_info.ai(:plain=>true)
 
       item name: "your friends",
         url: raw_url("/reader/api/0/friend/list")
 
       span.saved "Saved!"
-      pre @friends.ai(:plain=>true)
+
+      p "This list mixes together all your friends, followers and followed, for the time being."
+      
+      p "The plan is to show two lists, and to provide a 'bundle' or OPML export so you can continue to follow your friends."
+
+      @friends["friends"].each do |friend|
+        begin
+          user_info = {}
+          if friend["userIds"]
+            user_info["userId"] = friend["userIds"].first
+            user_info["userProfileId"] = friend["profileIds"].first
+            user_info["userName"] = friend["displayName"]
+            user_info["location"] = friend["location"]
+          end
+          # todo: use 'stream' directly -- maybe skip user_info altogether and use friend data to build bro
+          widget Bro, :user_info => user_info        
+        rescue => e
+          puts "#{e.class}: #{e.message}: #{e.backtrace[0..3].join("\n\t")}"
+          ap friend
+        end
+      end
+      
+      # pre @friends.ai(:plain=>true)
 
       hr
       
